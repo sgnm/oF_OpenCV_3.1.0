df878bb129322de6c31754e9c760c61cf08030a4
Shin <Shin@y-ito-15.private.i-studio.co.jp>
2016-03-04 18:57:06 +0900

    Add detect feature func from video cam

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -4,57 +4,90 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/features2d.hpp>
 
+using namespace cv;
+
 //--------------------------------------------------------------
 void ofApp::setup(){
     cout << "OpenCV version: " << CV_VERSION << endl;
     
-    //読み込む画像のパス
-    cv::String scene1_path = "/Users/Shin/Desktop/1.jpg";
-    cv::String scene2_path = "/Users/Shin/Desktop/2.jpg";
+//    //読み込む画像のパス
+//    cv::String scene1_path = "/Users/Shin/Desktop/2coin1.JPG";
+//    cv::String scene2_path = "/Users/Shin/Desktop/2coin2.JPG";
+//    
+//    //書き出す画像のパス
+//    cv::String scene_12_path = "/Users/Shin/Desktop/2coin12.jpg";
+//    
+//    
+//    //比較用画像を読み込む (アルファチャンネル非対応のため、IMREAD_COLORで強制する)
+//    cv::Mat scene1 = cv::imread(scene1_path, cv::IMREAD_COLOR);
+//    cv::Mat scene2 = cv::imread(scene2_path, cv::IMREAD_COLOR);
+//    
+//    //アルゴリズムにAKAZEを使用する
+//    auto algorithm = cv::AKAZE::create();
+//    
+//    // 特徴点抽出
+//    std::vector<cv::KeyPoint> keypoint1, keypoint2;
+//    algorithm->detect(scene1, keypoint1);
+//    algorithm->detect(scene2, keypoint2);
+//    
+//    // 特徴記述
+//    cv::Mat descriptor1, descriptor2;
+//    algorithm->compute(scene1, keypoint1, descriptor1);
+//    algorithm->compute(scene2, keypoint2, descriptor2);
+//    
+//    // マッチング (アルゴリズムにはBruteForceを使用)
+//    cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create("BruteForce");
+//    std::vector<cv::DMatch> match, match12, match21;
+//    matcher->match(descriptor1, descriptor2, match12);
+//    matcher->match(descriptor2, descriptor1, match21);
+//    //クロスチェック(1→2と2→1の両方でマッチしたものだけを残して精度を高める)
+//    for (size_t i = 0; i < match12.size(); i++)
+//    {
+//        cv::DMatch forward = match12[i];
+//        cv::DMatch backward = match21[forward.trainIdx];
+//        if (backward.trainIdx == forward.queryIdx)
+//        {
+//            match.push_back(forward);
+//        }
+//    }
+//    
+//    // マッチング結果の描画
+//    cv::Mat dest;
+//    cv::drawMatches(scene1, keypoint1, scene2, keypoint2, match, dest);
+//    
+//    //マッチング結果の書き出し
+//    cv::imwrite(scene_12_path, dest);
     
-    //書き出す画像のパス
-    cv::String scene_12_path = "/Users/Shin/Desktop/12.jpg";
+    VideoCapture cap(0); // open the default camera
+    if (!cap.isOpened()) // check if we succeeded
+        return -1;
+    Mat edges;
+    namedWindow("edges", 1);
     
+    Ptr<AKAZE> akaze = AKAZE::create();
     
-    //比較用画像を読み込む (アルファチャンネル非対応のため、IMREAD_COLORで強制する)
-    cv::Mat scene1 = cv::imread(scene1_path, cv::IMREAD_COLOR);
-    cv::Mat scene2 = cv::imread(scene2_path, cv::IMREAD_COLOR);
+    std::vector<cv::KeyPoint> keypoints;
+    cv::Mat descriptors;
     
-    //アルゴリズムにAKAZEを使用する
-    auto algorithm = cv::AKAZE::create();
+    cv::Mat image;
     
-    // 特徴点抽出
-    std::vector<cv::KeyPoint> keypoint1, keypoint2;
-    algorithm->detect(scene1, keypoint1);
-    algorithm->detect(scene2, keypoint2);
-    
-    // 特徴記述
-    cv::Mat descriptor1, descriptor2;
-    algorithm->compute(scene1, keypoint1, descriptor1);
-    algorithm->compute(scene2, keypoint2, descriptor2);
-    
-    // マッチング (アルゴリズムにはBruteForceを使用)
-    cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create("BruteForce");
-    std::vector<cv::DMatch> match, match12, match21;
-    matcher->match(descriptor1, descriptor2, match12);
-    matcher->match(descriptor2, descriptor1, match21);
-    //クロスチェック(1→2と2→1の両方でマッチしたものだけを残して精度を高める)
-    for (size_t i = 0; i < match12.size(); i++)
+    for (;;)
     {
-        cv::DMatch forward = match12[i];
-        cv::DMatch backward = match21[forward.trainIdx];
-        if (backward.trainIdx == forward.queryIdx)
-        {
-            match.push_back(forward);
-        }
+        Mat frame;
+        cap >> frame; // get a new frame from camera
+        cvtColor(frame, edges, CV_BGR2GRAY);
+        
+        akaze->detectAndCompute(edges, cv::noArray(), keypoints, descriptors);
+        
+        cv::drawKeypoints(edges, keypoints, edges);
+        
+        
+        
+        imshow("edges", edges);
+        if (waitKey(30) >= 0)
+            break;
     }
-    
-    // マッチング結果の描画
-    cv::Mat dest;
-    cv::drawMatches(scene1, keypoint1, scene2, keypoint2, match, dest);
-    
-    //マッチング結果の書き出し
-    cv::imwrite(scene_12_path, dest);
+    // the camera will be deinitialized automatically in VideoCapture destructor
 }
 
 //--------------------------------------------------------------
